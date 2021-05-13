2747103ebdc8c62ea3f3a8a55c15cc7006d9a4ac
Eric Charnesky <eric.charnesky@careevolution.com>
2021-05-12 20:16:28 -0400

    more vehicle examples

--- JavaReview/src/javareview/JavaReview.java
+++ JavaReview/src/javareview/JavaReview.java
@@ -3,7 +3,36 @@ package javareview;
 public class JavaReview {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Vehicle transit = new Vehicle("blue", 5.95, 90.8);
+
+        System.out.println(transit);
+
+        if (transit.addFuel(transit.getFuelTankCapacityInLiters())) {
+            System.out.println("You added " + 
+                    transit.getFuelTankCapacityInLiters() + " liters!");
+        } else {
+            System.out.println("You spilled the gas you idiot!");
+        }
+
+        System.out.println(transit);
+
+        if (transit.drive(48)) {
+            System.out.println("You drove to Ann Arbor!");
+        } else {
+            System.out.println("You ran out of gas you idiot!");
+        }
+
+        System.out.println(transit);
+
+        System.out.println("driving home");
+        if (transit.drive(48)) {
+            System.out.println("You drove Home!");
+        } else {
+            System.out.println("You ran out of gas you idiot!");
+        }
+        
+        System.out.println(transit);
+
     }
 
 }
--- JavaReview/src/javareview/Vehicle.java
+++ JavaReview/src/javareview/Vehicle.java
@@ -3,25 +3,55 @@ package javareview;
 public class Vehicle {
 
     private String color;
-    private double maxMilesPerHour;
+    private double kilometersPerLiter;
     private double fuelTankCapacityInLiters;
     private double currentFuelInTankInLiters;
 
-    public Vehicle(String color, double maxMilesPerHour, double fuelTankCapacityInLiters) {
+    public Vehicle(String color, double kilometersPerLiter, double fuelTankCapacityInLiters) {
         this.color = color;
-        this.maxMilesPerHour = maxMilesPerHour;
+        this.kilometersPerLiter = kilometersPerLiter;
         this.fuelTankCapacityInLiters = fuelTankCapacityInLiters;
         currentFuelInTankInLiters = 0;
     }
-    
-    //public boolean 
+
+    public boolean drive(double kilometersToDrive) {
+        if (kilometersToDrive < 0) {
+            return false;
+        }
+
+        double litersUsed = kilometersToDrive / kilometersPerLiter;
+
+        currentFuelInTankInLiters -= litersUsed;
+
+        if (currentFuelInTankInLiters < 0) {
+            currentFuelInTankInLiters = 0;
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean addFuel(double fuelToAddInLiters) {
+        if (fuelToAddInLiters < 0) {
+            return false;
+        }
+
+        currentFuelInTankInLiters += fuelToAddInLiters;
+
+        if (currentFuelInTankInLiters > fuelTankCapacityInLiters) {
+            currentFuelInTankInLiters = fuelTankCapacityInLiters;
+            return false;
+        }
+
+        return true;
+    }
 
     public String getColor() {
         return color;
     }
 
-    public double getMaxMilesPerHour() {
-        return maxMilesPerHour;
+    public double getKilometersPerLiter() {
+        return kilometersPerLiter;
     }
 
     public double getFuelTankCapacityInLiters() {
@@ -34,6 +64,12 @@ public class Vehicle {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s color vehicle with %.1f liters of fuel left",
+                color, currentFuelInTankInLiters);
     }
 
 }
