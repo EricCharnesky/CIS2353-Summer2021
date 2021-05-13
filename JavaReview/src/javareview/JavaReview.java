bbb7e1aee6324cb2bfca2e62e73c23df8d992c14
Eric Charnesky <eric.charnesky@careevolution.com>
2021-05-12 20:22:59 -0400

    arrays and arraylists

--- JavaReview/src/javareview/JavaReview.java
+++ JavaReview/src/javareview/JavaReview.java
@@ -1,5 +1,7 @@
 package javareview;
 
+import java.util.ArrayList;
+
 public class JavaReview {
 
     public static void main(String[] args) {
@@ -32,6 +34,42 @@ public class JavaReview {
         }
         
         System.out.println(transit);
+        
+        
+        ArrayList<String> kids;
+        kids = new ArrayList<>();
+        
+        kids.add("Joy");
+        kids.add("Jeb");
+        kids.add("Jenavieve");
+        kids.add("Journey");
+        kids.add("Jubilee");
+        kids.add("Jackson");
+        
+        String[] kidsArray = new String[6];
+        kidsArray[0] = "Joy";
+        kidsArray[1] = "Jeb";
+        kidsArray[2] = "Vivi";
+        kidsArray[3] = "Journey";
+        kidsArray[4] = "Jubilee";
+        kidsArray[5] = "Jack";
+        
+        for ( int index = 0; index < kids.size(); index++ ){
+            System.out.println(kids.get(index));
+        }
+        
+        for ( String kid : kids ){
+            System.out.println(kid);
+        }
+        
+        for ( int index = 0; index < kidsArray.length; index++ ){
+            System.out.println(kidsArray[index]);
+        }
+        
+        for ( String kid : kidsArray ){
+            System.out.println(kid);
+        }
+        
 
     }
 
