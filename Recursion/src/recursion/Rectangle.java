72bd8105adbac983c282e34711798da3f1472864
Eric Charnesky <eric.charnesky@careevolution.com>
2021-06-09 19:50:49 -0400

    recursion and exceptions

--- Recursion/src/recursion/Rectangle.java
+++ Recursion/src/recursion/Rectangle.java
@@ -5,12 +5,28 @@ public class Rectangle implements Comparable<Rectangle> {
     private int length;
     private int width;
 
+
     public Rectangle(int length, int width) {
-        this.length = length;
-        this.width = width;
+        setLength(length);
+        setWidth(width);
     }
 
-    public int getLength() {
+
+    public void setLength(int length) {
+        if ( length <= 0){
+            throw new IllegalArgumentException("Length cannot be 0 or less");
+        }
+        this.length = length;
+    }
+
+    public void setWidth(int width) {
+        if ( width <= 0){
+            throw new IllegalArgumentException("Width cannot be 0 or less");
+        }
+        this.width = width;
+    }
+    
+       public int getLength() {
         return length;
     }
 
--- Recursion/src/recursion/Recursion.java
+++ Recursion/src/recursion/Recursion.java
@@ -17,8 +17,65 @@ public class Recursion {
 
         Sudoku sudoku = new Sudoku(board);
 
-        sudoku.solve();
+        //sudoku.solve();
+        try {
+            Rectangle rectangle = new Rectangle(-1, 5);
+            System.out.println("area is: " + rectangle.getArea());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
 
+        for (int nth = 0; nth < 46; nth++) {
+            System.out.println(nth + ": " + fib(nth));
+        }
+
+    }
+
+    // less efficient than the iterative solution
+    private static int fibHelper(int nth, int currentNth, int previous, int current) {
+        if (nth == currentNth) {
+            return previous + current;
+        }
+        return fibHelper(nth, currentNth + 1, current, previous + current);
+    }
+
+    public static int fib(int nth) {
+        if (nth == 0) {
+            return 0;
+        }
+        if (nth == 1) {
+            return 1;
+        }
+        return fibHelper(nth, 2, 0, 1);
+    }
+
+    public static int interativeFib(int nth) {
+        if (nth == 0) {
+            return 0;
+        }
+        if (nth == 1) {
+            return 1;
+        }
+        int previous = 0;
+        int current = 1;
+        int currentNth = 2;
+        while (currentNth < nth) {
+            int next = previous + current;
+            previous = current;
+            current = next;
+            currentNth++;
+        }
+        return previous + current;
+    }
+
+    public static int badfib(int nth) {
+        if (nth == 0) {
+            return 0;
+        }
+        if (nth == 1) {
+            return 1;
+        }
+        return badfib(nth - 1) + badfib(nth - 2);
     }
 
 // essentially a loop
