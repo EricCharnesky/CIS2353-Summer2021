ad01e9e3282d258a65dd521822ccd854bc954c8f
Eric Charnesky <eric.charnesky@careevolution.com>
2021-07-21 18:54:29 -0400

    Project 4

---
+++ Project4/src/project4/Person.java
@@ -0,0 +1,62 @@
+package project4;
+
+import java.util.Random;
+
+public class Person implements Comparable<Person> {
+
+    public enum Type {
+        Nurse, Doctor, Other
+    };
+
+    private Type type;
+    private int timeEnteredLine;
+    private int timeOrderedWasReceived;
+
+    public Person() {
+        Random random = new Random();
+        int value = random.nextInt(3);
+        if (value == 0) {
+            type = Type.Nurse;
+        } else if (value == 1) {
+            type = Type.Doctor;
+        } else {
+            type = Type.Other;
+        }
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public int getTimeEnteredLine() {
+        return timeEnteredLine;
+    }
+
+    public int getTimeOrderedWasReceived() {
+        return timeOrderedWasReceived;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public void setTimeEnteredLine(int timeEnteredLine) {
+        this.timeEnteredLine = timeEnteredLine;
+    }
+
+    public void setTimeOrderedWasReceived(int timeOrderedWasReceived) {
+        this.timeOrderedWasReceived = timeOrderedWasReceived;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return type.name();
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return type.ordinal() - other.type.ordinal();
+    }
+
+}
---
+++ Project4/src/project4/Project4.java
@@ -0,0 +1,83 @@
+package project4;
+
+import java.util.PriorityQueue;
+import java.util.Random;
+import javax.crypto.interfaces.PBEKey;
+
+public class Project4 {
+
+    public static void main(String[] args) {
+        PriorityQueue<Person> queue = new PriorityQueue<>();
+        Random random = new Random();
+
+        boolean isSomeonesSandwichBeingMade = false;
+        int timeOrderIsCompleted = 0;
+
+        double totalTimeForNurses = 0;
+        int numberOfNursesServed = 0;
+        double totalTimeForDoctors = 0;
+        int numberOfDoctorsServed = 0;
+        double totalTimeForOthers = 0;
+        int numberOfOthersServed = 0;
+
+        Person beingServed = null;
+
+        for (int minute = 0; minute < 120; minute++) {
+            if (random.nextInt(3) == 0) {
+                Person person = new Person();
+                person.setTimeEnteredLine(minute);
+                queue.add(person);
+                System.out.println("New FTE entered the line: " + person);
+            }
+            if (minute == timeOrderIsCompleted) {
+                isSomeonesSandwichBeingMade = false;
+                System.out.println("Completed sandwich for " + beingServed + " at " + minute);
+            }
+            if (!isSomeonesSandwichBeingMade && !queue.isEmpty()) {
+                isSomeonesSandwichBeingMade = true;
+
+                beingServed = queue.peek();
+                queue.remove(beingServed);
+
+                System.out.println("Started making sandwich for " + beingServed + " at " + minute);
+
+                int numberOfTopping = random.nextInt(6);
+
+                timeOrderIsCompleted = minute + numberOfTopping + 1;
+
+                beingServed.setTimeOrderedWasReceived(timeOrderIsCompleted);
+
+                if (beingServed.getType() == Person.Type.Nurse) {
+                    totalTimeForNurses
+                            += beingServed.getTimeOrderedWasReceived() - beingServed.getTimeEnteredLine();
+                    numberOfNursesServed++;
+                } else if (beingServed.getType() == Person.Type.Doctor) {
+                    totalTimeForDoctors
+                            += beingServed.getTimeOrderedWasReceived() - beingServed.getTimeEnteredLine();
+                    numberOfDoctorsServed++;
+                } else {
+                    totalTimeForOthers
+                            += beingServed.getTimeOrderedWasReceived() - beingServed.getTimeEnteredLine();
+                    numberOfOthersServed++;
+                }
+            }
+        }
+        
+
+        System.out.println("Cafe closed after 120 minutes");
+        
+        System.out.println(queue.size() + " number of people were not served");
+        
+        System.out.println("Number of nurses serverd: " + numberOfNursesServed);
+        System.out.println("Average time from entering line to getting sandwich for nurses "
+                + totalTimeForNurses / numberOfNursesServed);
+        System.out.println("Number of doctors serverd: " + numberOfDoctorsServed);
+        System.out.println("Average time from entering line to getting sandwich for doctors "
+                + totalTimeForDoctors / numberOfDoctorsServed);
+        System.out.println("Number of others serverd: " + numberOfOthersServed);
+        System.out.println("Average time from entering line to getting sandwich for others "
+                + totalTimeForOthers / numberOfOthersServed);
+
+    }
+
+}
